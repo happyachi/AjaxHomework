02eb7cb47d39605873f7f4b571019ee5decf4fe6
happyachi <gn77233@gmail.com>
2024-01-23 09:07:33 +0800

    加入專案檔案。

---
+++ WebApplication2/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly MyDBContext _context;
+
+        public ApiController(MyDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cities()
+        {
+            var cities = _context.Addresses.Select(a => a.City);
+            return Json(cities);
+        }
+    }
+}
